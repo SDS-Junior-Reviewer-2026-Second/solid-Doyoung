e87070db6cff7425105dbd73eaf22ea3769bd1d3
kimdoyoung0319 <kimdoyoung0319@gmail.com>
2026-09-17 13:51:51 +0900

    Refactored Vehicle class

---
+++ src/main/java/vehicle/GasStation.java
@@ -0,0 +1,9 @@
+package vehicle;
+
+public class GasStation {
+
+    // this is not a car's responsibility.
+    public void reFuel(Vehicle vehicle) {
+        vehicle.setRemainingFuel(vehicle.getMaxFuel());
+    }
+}
